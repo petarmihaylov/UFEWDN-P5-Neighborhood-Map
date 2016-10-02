8378bea32cd11ea6f766493a31b81dbc99700efb
Petar Mihaylov <petar.m@studio350.com>
2016-10-01 23:08:55 -0400

    Build basic UI

--- project/app/scripts/main.js
+++ project/app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+$(function(){
+  // Load the map
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 26.100365, lng: -80.399775},
+      zoom: 13,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    });
+  };
+
+  initMap();
+});
