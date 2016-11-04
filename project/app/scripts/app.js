edf5f0e78ccfdd148d3e3f1703fe3eb663364cc6
Petar Mihaylov <petar.m@studio350.com>
2016-11-04 16:01:46 -0400

    Fix error handling implementation

--- project/app/scripts/app.js
+++ project/app/scripts/app.js
@@ -11,7 +11,8 @@ requirejs.config({
 
 // Load the main app module to start the app
 requirejs(['app/main'], function () {
-    //Do something with $ here
+    // Execution is initated automatically
+    // No need to do anthing here
 },
 
 function (err) {
@@ -19,10 +20,13 @@ function (err) {
   //The error has a list of modules that failed
   var failedId = err.requireModules && err.requireModules[0];
   if (failedId.indexOf('google') !== -1) {
-    alert('Google Maps API failed to load. Please try reloading the page.');
+    if(alert('Google Maps API failed to load.\n\nPlease try reloading the page.\n\n Click \'OK\' to try to reload the page.')){}
+    else window.location.reload();
   } else if (failedId.indexOf('firebase') !== -1) {
-    alert('Firebase API failed to load. Please try reloading the page.');
+    if(alert('Firebase API failed to load.\n\nPlease try reloading the page.\n\n Click \'OK\' to try to reload the page.')){}
+    else window.location.reload();
   } else {
-    alert('Something went wrong: ' + err.requireModules + ' Please try reloading the page.');
+    if(alert('Something went wrong:\n\n' + err.requireModules + '\n\nPlease try reloading the page.\n\n Click \'OK\' to try to reload the page.')){}
+    else window.location.reload();
   }
 });
